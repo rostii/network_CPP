bf5bbd1484bb140cb161871e9e6404d01b3d6d73
Rosti <rosti@live.de>
2021-04-17 11:52:15 +0200

    Initial Commit

---
+++ DNN_CPP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DNN_CPP
+//
+//  Created by Rostislaw Neumann on 17.04.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
